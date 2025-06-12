b6ced18396633574f4ffa2956228a6c2b93a7320
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-12 15:03:43 +0530

    Create function playRound
    Create the function with the game logic where
    rock beats scissors, paper beats rock and
    scissors beat paper.

--- app.js
+++ app.js
@@ -12,7 +12,32 @@ function getHumanChoice() {
   return prompt("Enter your choice!", "");
 }
 
+function playRound(humanChoice, computerChoice) {
+  humanChoice =
+    humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+  if (
+    (humanChoice === "Rock" && computerChoice === "Scissors") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissors" && computerChoice === "Paper")
+  ) {
+    return `You Win! ${humanChoice} beats ${computerChoice}`;
+  } else if (
+    (humanChoice === "Rock" && computerChoice === "Rock") ||
+    (humanChoice === "Paper" && computerChoice === "Paper") ||
+    (humanChoice === "Scissors" && computerChoice === "Scissors")
+  ) {
+    return "It's a draw.";
+  } else {
+    return `You Lose! ${computerChoice} beats ${humanChoice}`;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 let humanScore = 0;
 let computerScore = 0;
 
-
+console.log(computerSelection);
+console.log(playRound(humanSelection, computerSelection));
