b415cde32cb919a0b72c5741bd58781a2f9e163a
Deepak Dhiman <99280200+Deepak-Dhiman@users.noreply.github.com>
2025-06-12 16:59:21 +0530

    Create function playGame & edit function playRound
    Moved the variables humanScore & computerScore
    into the playGame function. Also modified the
    logic of the playRound function.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Gets computer choice
 function getComputerChoice() {
   if (Math.floor(Math.random() * 3) === 0) {
     return "Rock";
@@ -8,36 +9,50 @@ function getComputerChoice() {
   }
 }
 
+// Gets human choice
 function getHumanChoice() {
   return prompt("Enter your choice!", "");
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice =
-    humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+// Main calling function
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-  if (
-    (humanChoice === "Rock" && computerChoice === "Scissors") ||
-    (humanChoice === "Paper" && computerChoice === "Rock") ||
-    (humanChoice === "Scissors" && computerChoice === "Paper")
-  ) {
-    return `You Win! ${humanChoice} beats ${computerChoice}`;
-  } else if (
-    (humanChoice === "Rock" && computerChoice === "Rock") ||
-    (humanChoice === "Paper" && computerChoice === "Paper") ||
-    (humanChoice === "Scissors" && computerChoice === "Scissors")
-  ) {
-    return "It's a draw.";
+  // Main game logic
+  function playRound(humanChoice, computerChoice) {
+    humanChoice =
+      humanChoice.slice(0, 1).toUpperCase() +
+      humanChoice.slice(1).toLowerCase();
+
+    if (
+      (humanChoice === "Rock" && computerChoice === "Scissors") ||
+      (humanChoice === "Paper" && computerChoice === "Rock") ||
+      (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      return `You Win! Computer chose ${computerChoice}. ${humanChoice} beats ${computerChoice}. Score Human: ${++humanScore} Computer: ${computerScore}`;
+    } else if (
+      (humanChoice === "Rock" && computerChoice === "Rock") ||
+      (humanChoice === "Paper" && computerChoice === "Paper") ||
+      (humanChoice === "Scissors" && computerChoice === "Scissors")
+    ) {
+      return `It's a draw. Computer chose ${computerChoice}. Score Human: ${humanScore} Computer: ${computerScore}`;
+    } else {
+      return `You Lose! Computer chose ${computerChoice}. ${computerChoice} beats ${humanChoice}. Score Human: ${humanScore} Computer: ${++computerScore}`;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+  }
+
+  if (humanScore > computerScore) {
+    return `You won!. Final score => Human: ${humanScore} Computer: ${computerScore}`;
+  } else if (humanScore === computerScore) {
+    return `It's a draw. Final score => Human: ${humanScore} Computer: ${computerScore}`;
   } else {
-    return `You Lose! ${computerChoice} beats ${humanChoice}`;
+    return `You lost! Final score => Human: ${humanScore} Computer: ${computerScore}`;
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-let humanScore = 0;
-let computerScore = 0;
-
-console.log(computerSelection);
-console.log(playRound(humanSelection, computerSelection));
+console.log(playGame());
